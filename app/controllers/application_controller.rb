0fa493a54ab9dd1d21a689b7ca86f32958696f06
Michael Chen <michael.chen.1120@gmail.com>
2019-12-11 18:33:09 -0500

    Add users and sessions auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+
+    private
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
 end
