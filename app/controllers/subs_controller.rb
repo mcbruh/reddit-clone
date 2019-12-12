9e5f56753b35764602bd6e4d61c254a590e91e14
Michael Chen <michael.chen.1120@gmail.com>
2019-12-12 14:13:13 -0500

    Add controller actions and views for subs

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,27 +1,57 @@
 class SubsController < ApplicationController
 
-    def index
+    before_action :verify_moderator, only: [:edit, :update]
 
+    def index
+        @subs = Sub.all
+        render :index
     end
 
     def new
-        
+        @sub = Sub.new
+        render :new
     end
 
     def create
-
+        @sub = current_user.subs.new(sub_params)
+        if @sub.save
+            flash[:notice] = 'Sub successfully created'
+            redirect_to sub_url(@sub)
+        else
+            flash[:errors] = @sub.errors.full_messages
+            render :new
+        end
     end
 
     def show
-
+        @sub = Sub.find(params[:id])
+        render :show
     end
 
     def edit
-
+        @sub = Sub.find(params[:id])
+        render :edit
     end
 
     def update
+        @sub = Sub.find(params[:id])
+        if @sub.update_attributes
+            flash[:notice] = 'Sub updated'
+            redirect_to sub_url(@sub)
+        else
+            flash[:errors] = @sub.errors.full_messages
+            render :edit
+        end
+    end
 
+    private
+
+    def sub_params
+        params.require(:sub).permit(:title, :description, :moderator_id)
+    end
+
+    def verify_moderator
+        redirect_to subs_url unless current_user.id == self.moderator_id
     end
 
 end
