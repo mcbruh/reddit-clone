0279b8dfb203157ad89d0b251aec96f3134de041
Michael Chen <michael.chen.1120@gmail.com>
2019-12-11 19:08:13 -0500

    Add users and session routes. Add new user and log in form. Fix typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true }
-    after_intialize :ensure_session_token
+    after_initialize :ensure_session_token
 
     def password=(password)
         @password = password
