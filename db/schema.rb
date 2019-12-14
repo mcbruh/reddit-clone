9692cc274a0cac53151b5dbccdc2414c8b53f876
Michael Chen <michael.chen.1120@gmail.com>
2019-12-14 17:15:01 -0500

    Fix submit_button variable in form partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_001235) do
+ActiveRecord::Schema.define(version: 2019_12_14_211734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
